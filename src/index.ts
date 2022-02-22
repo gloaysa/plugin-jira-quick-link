77f675b4ddc4fdda70d315bf7f215eb9bbf08e04
Guillermo Loaysa <gloaysa@gmail.com>
2022-02-22 19:14:08 +0100

    add popup to manifest.json

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
-// Uncomment this block to reset the secret for debugging purposes
-/*
-	chrome.storage.sync.set({
-		pass: null,
-		secret: null
-	});
-	*/
-
 chrome.browserAction.onClicked.addListener(() => {
 	chrome.storage.sync.get(['baseUrl'], (key: { baseUrl: string; }) => {
 			if (!key.baseUrl) {
