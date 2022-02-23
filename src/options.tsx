c2f9f24c96243d725ef9916e21fb7d98a178ba01
Guillermo Loaysa <gloaysa@gmail.com>
2022-02-23 10:31:45 +0100

    add styles to popup

--- src/options.tsx
+++ src/options.tsx
@@ -1,129 +1,133 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom';
 
 const Options = () => {
-	const [baseUrl, setBaseUrl] = useState<string>('');
+  const [baseUrl, setBaseUrl] = useState<string>('');
 
-	const [status, setStatus] = useState<string>('');
+  const [status, setStatus] = useState<string>('');
 
-	useEffect(() => {
-		chrome.storage.sync.get(['baseUrl'],(key: { baseUrl: string }) => {
-				if (!baseUrl) {
-					setBaseUrl(key.baseUrl ? key.baseUrl : '');
-				}
-			}
-		);
-	});
+  useEffect(() => {
+    chrome.storage.sync.get(['baseUrl'], (key: { baseUrl: string }) => {
+        if (!baseUrl) {
+          setBaseUrl(key.baseUrl ? key.baseUrl : '');
+        }
+      }
+    );
+  });
 
-	const openShortCutsInChrome = () => {
-		chrome.tabs.create({url: 'chrome://extensions/shortcuts'})
-	}
+  const openShortCutsInChrome = () => {
+    chrome.tabs.create({url: 'chrome://extensions/shortcuts'})
+  }
 
-	const saveOptions = () => {
-		// Saves options to chrome.storage.sync.
-		chrome.storage.sync.set({ baseUrl: baseUrl }, () => {
-			let timeoutId;
+  const saveOptions = () => {
+    // Saves options to chrome.storage.sync.
+    chrome.storage.sync.set({baseUrl: baseUrl}, () => {
+      let timeoutId;
 
-			setStatus('Options saved.');
-			timeoutId = setTimeout(() => {
-				setStatus(undefined);
-			}, 1000);
-			return () => clearTimeout(timeoutId);
-		});
-	};
+      setStatus('Options saved.');
+      timeoutId = setTimeout(() => {
+        setStatus(undefined);
+      }, 1000);
+      return () => clearTimeout(timeoutId);
+    });
+  };
 
-	const displayNotification = () => {
-		if (status) {
-			return <div className="notification is-primary">{status}</div>;
-		}
-		return (
-			<div className="field is-centered">
-				<button className="button is-info is-medium is-fullwidth" onClick={saveOptions} disabled={!baseUrl}>
-					Guardar
-				</button>
-			</div>
-		);
-	};
+  const displayNotification = () => {
+    if (status) {
+      return <div className="notification is-primary">{status}</div>;
+    }
+    return (
+      <div className="field is-centered">
+        <button className="button is-info is-medium is-fullwidth" onClick={saveOptions} disabled={!baseUrl}>
+          Save
+        </button>
+      </div>
+    );
+  };
 
-	return (
-		<>
-			<div className="container">
-				<section className="articles">
-					<div className="columns is-mobile">
-						<div className="column is-12-mobile is-8-desktop is-offset-2-desktop">
-							{/* START ARTICLE */}
-							<div className="card article">
-								<div className="card-content">
-									<div className="columns is-mobile is-centered is-vcentered">
-										<div className="column is-narrow">
-											<img src="../images/icons/60.png" alt="Jira Quick Link" />
-										</div>
-										<div className="column is-narrow">
-											<span className="title">Jira Quick Link</span>
-										</div>
-									</div>
+  return (
+    <>
+      <div className="container">
+        <section className="articles">
+          <div className="columns is-mobile">
+            <div className="column is-12-mobile is-8-desktop is-offset-2-desktop">
+              {/* START ARTICLE */}
+              <div className="card article">
+                <div className="card-content">
+                  <div className="columns is-mobile is-centered is-vcentered">
+                    <div className="column is-narrow">
+                      <img src="../images/icons/60.png" alt="Jira Quick Link"/>
+                    </div>
+                    <div className="column is-narrow">
+                      <span className="title">Jira Quick Link</span>
+                    </div>
+                  </div>
 
-									<div className="field is-centered">
-										<label className="label">Base URL</label>
-										<div className="control has-icons-right">
-											<input
-												className="input"
-												type='text'
-												placeholder="https://issues.apache.org/"
-												onChange={(event) => setBaseUrl(event.target.value)}
-												value={baseUrl}
-											/>
-										</div>
-									</div>
+                  <div className="field is-centered">
+                    <label className="label">Base URL</label>
+                    <div className="control has-icons-right">
+                      <input
+                        className="input"
+                        type='text'
+                        placeholder="https://issues.apache.org/"
+                        onChange={(event) => setBaseUrl(event.target.value)}
+                        value={baseUrl}
+                      />
+                    </div>
+                  </div>
+                  <br/>
 
-									<br />
+                  {displayNotification()}
 
-									To make this plugin even better, navigate to <a href="" onClick={openShortCutsInChrome}>Chrome Shortcuts </a>
-									 and add a custom keyboard shortcut to open it (I recommend Cmd + Control + J on MacOS)
+                  <hr/>
 
-									<br />
-									<br />
+                  <p>
+                    <strong>Save your changes before setting the shortcut</strong>
+                  </p>
+                  <p>
+                    To make this plugin even better, navigate to <a href="" onClick={openShortCutsInChrome}>Chrome
+                    Shortcuts </a>
+                    and add a custom keyboard shortcut to open it (I recommend <code>Cmd + Control + J on MacOS</code>).
+                  </p>
 
-									{displayNotification()}
+                  <br/>
 
-									<hr />
+                  <div className="columns is-mobile is-centered">
+                    <div className="column is-half">
+                      <div className="image is-centered">
+                        <img src="https://media.giphy.com/media/3KC2jD2QcBOSc/giphy.gif" alt="party"/>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* END ARTICLE --> */}
+            </div>
+            {/* END COLUMN --> */}
+          </div>
+        </section>
+        {/* END ARTICLE FEED */}
+      </div>
 
-									<div className="columns is-mobile is-centered">
-										<div className="column is-half">
-											<div className="image is-centered">
-												<img src="https://media.giphy.com/media/3KC2jD2QcBOSc/giphy.gif" alt="party" />
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							{/* END ARTICLE --> */}
-						</div>
-						{/* END COLUMN --> */}
-					</div>
-				</section>
-				{/* END ARTICLE FEED */}
-			</div>
-
-			<footer className="footer">
-				<div className="content has-text-centered">
-					<p>
-						<strong>Jira Quick Link</strong> - made with <span className="has-text-danger">&hearts;</span> by{' '}
-						<a href="https://loaysa.com">Guillermo Loaysa</a>
-					</p>
-					<div className="tags has-addons level-item">
-						<span className="tag is-rounded is-info">last update</span>
-						<span className="tag is-rounded">March, 2022</span>
-					</div>
-				</div>
-			</footer>
-		</>
-	);
+      <footer className="footer">
+        <div className="content has-text-centered">
+          <p>
+            <strong>Jira Quick Link</strong> - made with <span className="has-text-danger">&hearts;</span> by{' '}
+            <a href="https://loaysa.com">Guillermo Loaysa</a>
+          </p>
+          <div className="tags has-addons level-item">
+            <span className="tag is-rounded is-info">last update</span>
+            <span className="tag is-rounded">March, 2022</span>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 ReactDOM.render(
-	<React.StrictMode>
-		<Options />
-	</React.StrictMode>,
-	document.getElementById('root')
+  <React.StrictMode>
+    <Options/>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,48 +1,50 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
 const Popup = () => {
-	const url = 'jira/browse/';
+  const url = 'jira/browse/';
 
-	const [baseUrl, setBaseUrl] = useState<string>();
-	const [jiraTicket, setJiraTicket] = useState<string>();
+  const [baseUrl, setBaseUrl] = useState<string>();
+  const [jiraTicket, setJiraTicket] = useState<string>();
 
-	const goToOptions = () => {
-		chrome.runtime.openOptionsPage();
-	};
+  const goToOptions = () => {
+    chrome.runtime.openOptionsPage();
+  };
 
-	const goToJiraTicket = () => {
-		if (jiraTicket) {
-			chrome.tabs.create({url: `${baseUrl}${url}/${jiraTicket}`})
-		}
-	}
+  const goToJiraTicket = () => {
+    if (jiraTicket) {
+      chrome.tabs.create({url: `${baseUrl}${url}/${jiraTicket}`})
+    }
+  }
 
-	chrome.storage.sync.get(['baseUrl'], (key: { baseUrl: string; }) => {
-			if (!key.baseUrl) {
-				return goToOptions();
-			}
+  chrome.storage.sync.get(['baseUrl'], (key: { baseUrl: string; }) => {
+      if (!key.baseUrl) {
+        return goToOptions();
+      }
 
-			setBaseUrl(key.baseUrl);
-		}
-	);
+      setBaseUrl(key.baseUrl);
+    }
+  );
 
-	return (
-		<div style={{ minWidth: '300px' }}>
-			<form>
-				<input autoFocus placeholder='PROJECT-XXXX' onChange={(event) => setJiraTicket(event.target.value)} type="text"/>
-				<button disabled={!jiraTicket} onClick={goToJiraTicket}>Go</button>
-			</form>
-
-			<button onClick={goToOptions} style={{ marginRight: '5px' }}>
-				Options
-			</button>
-		</div>
-	);
+  return (
+    <form className='has-text-centered'>
+      <div className="field has-addons">
+        <div className="control is-expanded">
+          <input className='input' autoFocus placeholder='PROJECT-XXXX'
+                 onChange={(event) => setJiraTicket(event.target.value)} type="text"/>
+        </div>
+        <div className="control">
+          <button className='button is-info' disabled={!jiraTicket} onClick={goToJiraTicket}>Go</button>
+        </div>
+      </div>
+      <p className="is-size-7"><a onClick={goToOptions}>Jira Quick Link</a> by <a className='has-text-black' target='__blank' href="https://loaysa.com">Guillermo Loaysa</a></p>
+    </form>
+  );
 };
 
 ReactDOM.render(
-	<React.StrictMode>
-		<Popup />
-	</React.StrictMode>,
-	document.getElementById('root')
+  <React.StrictMode>
+    <Popup/>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
